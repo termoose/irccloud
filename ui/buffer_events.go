0231e14f5abbc4f92b0d631848b50534db537457
Ole Andre Birkedal <birkedal@extab.net>
2019-11-25 16:14:42 +0100

    Properly escape all buffer events to avoid accidental color tags

--- ui/buffer_events.go
+++ ui/buffer_events.go
@@ -13,7 +13,7 @@ func (v *View) ChangeUserNick(channel, oldnick, newnick string, time int64, bid 
 		if err == nil {
 			ts := getTimestamp(time)
 			c.users.SetItemText(index, newnick, newnick)
-			line := fmt.Sprintf("[-:-:d]%s[-:-:-]  [coral]%s[-:-:-] is now known as [gold]%s[-:-:-]", ts, oldnick, newnick)
+			line := fmt.Sprintf("[-:-:d]%s[-:-:-]  [coral]%s[-:-:-] is now known as [gold]%s[-:-:-]", ts, tview.Escape(oldnick), tview.Escape(newnick))
 
 			v.writeToBuffer(line, c)
 		}
@@ -26,7 +26,7 @@ func (v *View) AddQuitEvent(channel, nick, hostmask, reason string, time int64, 
 
 		if c != nil {
 			ts := getTimestamp(time)
-			line := fmt.Sprintf("[-:-:d]%s[-:-:-][blueviolet]  <- [blueviolet:-:b]%s[-:-:-] quit (%s): [blueviolet]%s[-:-:-]", ts, nick, hostmask, reason)
+			line := fmt.Sprintf("[-:-:d]%s[-:-:-][blueviolet]  <- [blueviolet:-:b]%s[-:-:-] quit (%s): [blueviolet]%s[-:-:-]", ts, tview.Escape(nick), hostmask, tview.Escape(reason))
 			v.writeToBuffer(line, c)
 		}
 	})
@@ -38,7 +38,7 @@ func (v *View) AddPartEvent(channel, nick, hostmask string, time int64, bid int)
 
 		if c != nil {
 			ts := getTimestamp(time)
-			line := fmt.Sprintf("[-:-:d]%s[-:-:-][blueviolet]  <- [blueviolet:-:b]%s[-:-:-] left (%s)", ts, nick, hostmask)
+			line := fmt.Sprintf("[-:-:d]%s[-:-:-][blueviolet]  <- [blueviolet:-:b]%s[-:-:-] left (%s)", ts, tview.Escape(nick), hostmask)
 			v.writeToBuffer(line, c)
 		}
 	})
@@ -50,7 +50,7 @@ func (v *View) AddJoinEvent(channel, nick, hostmask string, time int64, bid int)
 
 		if c != nil {
 			ts := getTimestamp(time)
-			line := fmt.Sprintf("[-:-:d]%s[-:-:-][aquamarine]  -> [aquamarine:-:b]%s[-:-:-] joined (%s)", ts, nick, hostmask)
+			line := fmt.Sprintf("[-:-:d]%s[-:-:-][aquamarine]  -> [aquamarine:-:b]%s[-:-:-] joined (%s)", ts, tview.Escape(nick), hostmask)
 			v.writeToBuffer(line, c)
 		}
 	})
@@ -76,8 +76,6 @@ func (v *View) AddBufferMsg(channel, from, msg string, time int64, bid int) {
 }
 
 func (v *View) writeToBuffer(line string, c *channel) {
-	//v.app.QueueUpdateDraw(func() {
 	_, _ = c.chat.Write([]byte("\n" + line))
 	c.chat.ScrollToEnd()
-	//})
 }
