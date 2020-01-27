15dd67fe124384bfd1737bf0bd768d3320b13354
Ole Andre Birkedal <birkedal@extab.net>
2020-01-27 19:52:57 +0100

    Some tight locking, reduced startup drawing time

--- ui/activity_bar.go
+++ ui/activity_bar.go
@@ -7,6 +7,7 @@ import (
 	"github.com/rivo/tview"
 	"sort"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -22,6 +23,7 @@ type activityBar struct {
 	buffers      map[string]activityBuffer
 	sorted       []activityBuffer
 	triggerWords []string
+	buffersLock  sync.Mutex
 }
 
 func NewActivityBar(triggers []string) *activityBar {
@@ -35,7 +37,10 @@ func NewActivityBar(triggers []string) *activityBar {
 	}
 }
 
-func (b *activityBar) updateActivityBar() {
+func (b *activityBar) updateActivityBar(view *View) {
+	b.buffersLock.Lock()
+	defer b.buffersLock.Unlock()
+
 	list := []activityBuffer{}
 	for _, b := range b.buffers {
 		list = append(list, b)
@@ -47,7 +52,9 @@ func (b *activityBar) updateActivityBar() {
 
 	b.sorted = list
 
-	b.bar.SetText(generateBar(list))
+	view.app.QueueUpdate(func() {
+		b.bar.SetText(generateBar(list))
+	})
 }
 
 func bufferToBarElement(buffer activityBuffer) string {
@@ -86,22 +93,26 @@ func (b *activityBar) hasTriggerWord(line string) bool {
 }
 
 func (b *activityBar) MarkAsVisited(buffer string, view *View) {
+	b.buffersLock.Lock()
+	elem, ok := b.buffers[buffer]
+	b.buffersLock.Unlock()
+
+	if ok {
+		elem.visited = true
+
+		// If you have buffers open with activity from 34 years ago
+		// then this is going to look weird! Not sure of a better way
+		// to keep the order than a translation down memory lane
+		elem.lastActivity = elem.lastActivity.AddDate(-34, 0, 0)
+
+		// Assign the value back to the map
+		b.buffersLock.Lock()
+		b.buffers[buffer] = elem
+		b.buffersLock.Unlock()
+	}
+
 	view.app.QueueUpdateDraw(func() {
-		elem, ok := b.buffers[buffer]
-
-		if ok {
-			elem.visited = true
-
-			// If you have buffers open with activity from 34 years ago
-			// then this is going to look weird! Not sure of a better way
-			// to keep the order than a translation down memory lane
-			elem.lastActivity = elem.lastActivity.AddDate(-34, 0, 0)
-
-			// Assign the value back to the map
-			b.buffers[buffer] = elem
-		}
-
-		b.updateActivityBar()
+		b.updateActivityBar(view)
 	})
 }
 
@@ -114,16 +125,16 @@ func (b *activityBar) GetLatestActiveChannel() (string, error) {
 }
 
 func (b *activityBar) RegisterActivity(buffer, msg string, view *View) {
-	view.app.QueueUpdate(func() {
-		trigger := b.hasTriggerWord(msg)
+	trigger := b.hasTriggerWord(msg)
 
-		b.buffers[buffer] = activityBuffer{
-			displayName:  buffer,
-			lastActivity: time.Now(),
-			visited:      false,
-			triggerWord:  trigger,
-		}
+	b.buffersLock.Lock()
+	b.buffers[buffer] = activityBuffer{
+		displayName:  buffer,
+		lastActivity: time.Now(),
+		visited:      false,
+		triggerWord:  trigger,
+	}
+	b.buffersLock.Unlock()
 
-		b.updateActivityBar()
-	})
+	b.updateActivityBar(view)
 }
